5a46be7b7f346026ede56c44e4054e7358e24bc5
Bhargav <95201460+bhargavesh-dakka@users.noreply.github.com>
2024-08-02 11:24:28 +0530

    Update show_projects.py

--- pages/show_projects.py
+++ pages/show_projects.py
@@ -5,11 +5,10 @@ import mysql.connector as conn
 
 connection = conn.connect(
   host="sql12.freesqldatabase.com",
-  user="sql12625860",
-  password="pZetepGTxl",
+  user = st.secrets["username"],
+  password=st.secrets["password"],
   database = "sql12625860"
 )
-
 
 data = pd.read_sql("SELECT * FROM ProjectDetails;",connection)
 st.header("Your project details are \n")
